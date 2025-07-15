496fc83257ec9a0c6e7ea1141f113ced4a24dece
Engineer Ali <157731592+AliHassanRao@users.noreply.github.com>
2025-07-15 23:34:54 +0500

    Updated

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,18 @@ dotenv.config();
 
 const app = express();
 
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "https://yodo-recorder.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
 app.use(express.json());
-app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 const mongoUrl = process.env.MONGODB_URL;
